e4dad26dd54878ef1a62aafa1c39630882ee60ea
lhwjenny <lhwjenny@naver.com>
2025-06-14 18:51:00 +0900

    create ec2

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,15 @@
-# output "instance_id" {
-#   value = aws_instance.web.id
-# }
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "security_group_id" {
+  value = aws_security_group.default_sg.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
