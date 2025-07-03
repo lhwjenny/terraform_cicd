2bccadf7b6c1d2770c462d376a7dd290a022dc2d
이혜원 사원 클라우드서비스1파트 (yeslee@biztechi.com <yeslee@cns.lgcns.com>
2025-07-03 11:05:02 +0900

    test add service sg

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,16 +1,47 @@
-variable "vpc_id" {
-  type = string
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
 }
 
-variable "public_subnet_cidrs" {
-  type = list(string)
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "demo"
 }
 
-variable "private_subnet_cidrs" {
-  type = list(string)
+variable "default_sg_ingress_cidrs" {
+  description = "CIDR blocks for default SG ingress"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
 }
 
-variable "tag" {
-  type    = string
-  default = "Terraform"
+variable "service_sg_ingress_cidrs" {
+  description = "CIDR blocks for Athena service SG ingress"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_dns_enabled" {
+  description = "Enable Private DNS for VPC Endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "endpoint_services" {
+  description = "Set of endpoint service names (ex: ssm, sts, ec2, monitoring, athena)"
+  type        = set(string)
+  default     = ["ssm", "sts", "ec2", "monitoring", "athena"]
+}
+
+variable "athena_service_name" {
+  description = "Service name for Athena"
+  type        = string
+  default     = "athena"
 }
