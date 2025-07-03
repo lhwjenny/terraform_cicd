99ee3e77fec8d499f3416c3208f2460c539ceb76
이혜원 사원 클라우드서비스1파트 (yeslee@biztechi.com <yeslee@cns.lgcns.com>
2025-07-03 11:13:19 +0900

    fix error

---
+++ main.tf
@@ -0,0 +1,28 @@
+# terraform {
+#   required_version = ">= 1.0.5"
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 3.49.0"
+#     }
+#   }
+# }
+
+provider "aws" {
+  region = var.region
+}
+
+module "network" {
+  source = "./modules/network"
+
+  # 아래 변수들은 modules/network/variables.tf에 정의된 변수명에 맞게 전달해야 합니다.
+  region                   = var.region
+  name_prefix              = var.name_prefix
+  default_sg_ingress_cidrs = var.default_sg_ingress_cidrs
+  service_sg_ingress_cidrs = var.service_sg_ingress_cidrs
+  private_dns_enabled      = var.private_dns_enabled
+  common_tags              = var.common_tags
+  endpoint_services        = var.endpoint_services
+  athena_service_name      = var.athena_service_name
+}
